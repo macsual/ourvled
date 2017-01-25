26669b18470353915908cceb407978312ecb9213
Romario Maxwell <romario.maxwell@gmail.com>
2017-01-25 03:09:42 -0500

    Improve code handling config file buffer

--- src/ovle_config.c
+++ src/ovle_config.c
@@ -10,6 +10,7 @@
 #include <arpa/inet.h>
 
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #include "config.h"
 
@@ -34,61 +35,67 @@ struct ovle_parse {
     char *value_end;
 };
 
-static int ovle_parse_config_line(const char *buf, struct ovle_parse *c);
+static int ovle_parse_config_token(const char *buf, size_t len, struct ovle_parse *c);
 
 static int
-ovle_parse_config_line(const char *buf, struct ovle_parse *c)
+ovle_parse_config_token(const char *buf, size_t len, struct ovle_parse *c)
 {
     unsigned char ch;
     const unsigned char *p;
     enum {
         sw_start,
         sw_field,
+        sw_space_after_field,
         sw_space_before_value,
-        sw_value,
+        sw_value
     } state;
 
     state = sw_start;
 
-    for (p = buf; /* TODO */; p++) {
+    for (p = buf; ; p++) {
         ch = *p;
+
+        if ((p - (unsigned char *) buf) >= len)
+            return 3;
 
         switch (state) {
             /* first char */
             case sw_start:
-                c->field_start = (char *)p;
-
-                switch (ch) {
-                    case ';':
-                        c->field_end = (char *)p;
-                        return 3;
-
-                    default:
-                        state = sw_field;
-
-                        if (ch == '\0')
-                            return -1;
-
-                        break;
-                }
+                c->field_start = (char *) p;
+                state = sw_field;
                 break;
 
             case sw_field:
-                if (ch == '=') {
-                    c->field_end = (char *)p;
-                    state = sw_space_before_value;
-                    break;
+                switch (ch) {
+                    case ' ':
+                        c->field_end = (char *) p;
+                        state = sw_space_after_field;
+                        break;
+
+                    case '=':
+                        c->field_end = (char *) p;
+                        state = sw_space_before_value;
+                        break;
+
+                    case ';':
+                        return -1;
                 }
 
-                if (ch == ';') {
-                    c->field_end = (char *)p;
-                    c->value_start = (char *)p;
-                    c->value_end = (char *)p;
-                    goto done;
-                }
+                break;
 
-                if (ch == '\0')
-                    return -1;
+            case sw_space_after_field:
+                switch (ch) {
+                    case ' ':
+                        break;
+
+                    case ';':
+                        return -1;
+
+                    default:
+                        c->value_start = (char *) p;
+                        state = sw_value;
+                        break;
+                }
 
                 break;
 
@@ -98,32 +105,23 @@ ovle_parse_config_line(const char *buf, struct ovle_parse *c)
                         break;
 
                     case ';':
-                        c->value_start = (char *)p;
-                        c->value_end = (char *)p;
-                        goto done;
-
-                    case '\0':
                         return -1;
 
                     default:
-                        c->value_start = (char *)p;
+                        c->value_start = (char *) p;
                         state = sw_value;
                         break;
                 }
+
                 break;
 
             case sw_value:
                 switch (ch) {
-                    case ' ':
-                        break;
-
                     case ';':
-                        c->value_end = (char *)p;
+                        c->value_end = (char *) p;
                         goto done;
-
-                    case '\0':
-                        return -1;
                 }
+
                 break;
         }
     }
@@ -139,6 +137,7 @@ ovle_read_config(void)
     int fd;
     int rv;
     char *p;
+    off_t file_size;
     ssize_t bytes;
     size_t field_len, val_len;
     char *field, *value;
@@ -146,6 +145,7 @@ ovle_read_config(void)
     struct ovle_parse c;
     char *home_path;
     char conf_file_path[PATH_MAX];
+    struct stat file_info;
 
     home_path = getenv("HOME");
     if (home_path == NULL)
@@ -155,7 +155,19 @@ ovle_read_config(void)
 
     fd = open(conf_file_path, O_RDONLY);
     if (fd == -1) {
-        fprintf(stderr, "failed to parse config file\n");
+        fprintf(stderr, "open() \"%s\" failed\n", conf_file_path);
+        return -1;
+    }
+
+    if (fstat(fd, &file_info) == -1) {
+        fprintf(stderr, "fstat() \"%s\" failed\n", conf_file_path);
+        return -1;
+    }
+
+    file_size = file_info.st_size;
+
+    if (file_size > sizeof buf) {
+        fprintf(stderr, "config file too large\n");
         return -1;
     }
 
@@ -170,13 +182,21 @@ ovle_read_config(void)
         return -1;
     }
 
+    if (bytes != file_size) {
+        fprintf(stderr, "read() returned only %ld of %ld bytes\n", bytes, file_size);
+        return -1;
+    }
+
     p = buf;
 
     for (;;) {
-        rv = ovle_parse_config_line(p, &c);
+        rv = ovle_parse_config_token(p, file_size, &c);
 
         if (rv == 3)
             break;
+
+        if (rv == -1)
+            return -1;
 
         if (rv == 0) {
             field_len = c.field_end - c.field_start;
